ae5bd683217c47f02cc1d8d409dd7be90ea230de
Alex Krutskikh <krutskih.ay@gmail.com>
2024-10-09 01:28:34 +0300

    add_Twilio

--- questions/urls.py
+++ questions/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.urls import path
 from .views import add_question
 
+"""API for saving a question"""
+
 urlpatterns = [
     path('api/add/', add_question, name='add_question'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- questions/views.py
+++ questions/views.py
@@ -1,19 +1,20 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from .models import Question
+
+"""Function for saving a question"""
 
 @csrf_exempt  # Для упрощения, но лучше использовать токены CSRF
 def add_question(request):
     if request.method == 'POST':
-        # Получаем данные из запроса
+
         question_text = request.POST.get('question')
         pet_art = request.POST.get('petArt')
         pet_weight = request.POST.get('petWeight')
         pet_gender = request.POST.get('petGender')
         is_homeless = request.POST.get('isHomeless') == 'true'
 
-        # Создаем новый объект вопроса
+
         question = Question.objects.create(
             question=question_text,
             pet_art=pet_art,
